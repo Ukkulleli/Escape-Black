d937ce6bbf8c3377317783e7a04bee9d68b63ff8
Ukkulleli <135714671+Ukkulleli@users.noreply.github.com>
2024-11-30 22:28:39 +0100

    Movement gefixed

--- player.gd
+++ player.gd
@@ -1,32 +1,51 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 200
-const ACL = 1500
-const FRICTION = 600
+@onready var animated_sprite = $AnimatedSprite2D
+
+const speed = 100
 
 var input = Vector2.ZERO
+var direction = "down"
 
 func _physics_process(delta):
-	movement(delta)
+	input.x = Input.get_axis("left","right")
+	input.y = Input.get_axis("up","down")
+	
+	if input:
+		velocity = input * speed
+	else:
+		velocity = Vector2.ZERO
+	animate()
+	move_and_slide()
+
+func animate():
+	input = get_input()
+	if input.x == 0 and input.y == 0:
+		play_idle()
+	if input.x == 0 and input.y == 1:
+		animated_sprite.play("run_down")
+		direction = "down"
+	if input.x == 0 and input.y == -1:
+		animated_sprite.play("run_up")
+		direction = "up"
+	if input.x == 1 and input.y == 0:
+		animated_sprite.play("run_right")
+		direction = "right"
+	if input.x == -1 and input.y == 0:
+		animated_sprite.play("run_left")
+		direction = "left"
+
+func play_idle():
+	if direction == "down":
+		animated_sprite.play("idle_down")
+	if direction == "up":
+		animated_sprite.play("idle_up")
+	if direction == "left":
+		animated_sprite.play("idle_left")
+	if direction == "right":
+		animated_sprite.play("idle_right")
 
 func get_input():
 	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	return input.normalized()
-
-func movement(delta):
-	input = get_input()
-	
-	if input == Vector2.ZERO:
-		# Bremsen bei Stillstand
-		if velocity.length() > (FRICTION * delta):
-			velocity -= velocity.normalized() * (FRICTION * delta)
-		else:
-			velocity = Vector2.ZERO
-	else:
-		# Beschleunigung bei Eingabe
-		velocity += (input * ACL * delta)
-		velocity = velocity.limit_length(MAX_SPEED)
-	
-	# Bewegung anwenden
-	move_and_slide()
+	return input
