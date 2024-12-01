05d260d22bf0486a97042b8255111df03cfecc53
Ukkulleli <135714671+Ukkulleli@users.noreply.github.com>
2024-12-01 19:31:56 +0100

    Arman

--- start_screen.gd
+++ start_screen.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("attack"):
+		label.text = "Spiel wird gestartet..."
+		await get_tree().create_timer(3).timeout
 		get_tree().change_scene_to_file("res://node_2d.tscn")
+	
