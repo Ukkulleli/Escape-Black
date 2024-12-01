890b660fda491dbf442c5b0ed0eb1c40e0f75bb5
Ukkulleli <135714671+Ukkulleli@users.noreply.github.com>
2024-12-01 22:17:05 +0100

    chicken movement

--- chicken.gd
+++ chicken.gd
@@ -4,19 +4,45 @@ extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
 var movement = Vector2.ZERO
 var character_position = Vector2.ZERO
+var timer = 0
+var seated = false
 
 func _ready():
 	pass
-
 func _physics_process(delta):
 	#Erstellt einen Vektor, der in die Richtung des Spielers zeigt
 	movement = (player.get_character_position() - self.position)
-	animate()
+	
 	#Nähert sich bis auf die Nähe 50
 	if movement.length() > 35:
 		velocity = movement
-		move_and_slide()
+		timer = 0
+	elif movement.length() < 20:
+		velocity.x = 50 
+	else:
+		velocity = Vector2.ZERO
+		timer += 1
+	print(timer)
+	
+	
+	animate()
+	move_and_slide()
 
 func animate():
-	if movement.x == 0 and movement.y == 0:
-		animated_sprite.play("idle")
+	if velocity.x == 0 and velocity.y == 0:
+		if timer >= 85 and seated == false:
+			animated_sprite.play("sit")
+			seated = true
+		elif !seated:
+			animated_sprite.play("idle")
+	else:
+		if seated:
+			animated_sprite.play("stand_up")
+			if animated_sprite.animation_finished:
+				seated = false
+		elif !seated:
+			if velocity.x < 0:
+				animated_sprite.flip_h = false
+			elif velocity.x > 0:
+				animated_sprite.flip_h = true
+			animated_sprite.play("run")
