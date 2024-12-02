23bfcf2bd68d0bd10a2a2e18c2a983dd1e08bb2a
Ukkulleli <135714671+Ukkulleli@users.noreply.github.com>
2024-12-02 11:05:30 +0100

    Progress Bar added

--- start_screen.gd
+++ start_screen.gd
@@ -1,16 +1,23 @@
 extends Node2D
 
 @onready var label = $Label
+@onready var progress_bar = $ProgressBar
 
+var progress = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	progress_bar.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("attack"):
 		label.text = "Spiel wird gestartet..."
-		await get_tree().create_timer(2).timeout
+		progress_bar.visible = true
+		while progress <= 100:
+			progress_bar.value = progress
+			progress += 1
+			await get_tree().create_timer(0.01).timeout
+		
 		get_tree().change_scene_to_file("res://node_2d.tscn")
 	
